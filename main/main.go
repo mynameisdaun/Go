00aff0f6e656f0689c938656609712206698695f
mynameisdaun <daun9870.jung@gmail.com>
2021-10-27 20:27:40 +0900

    len(str) 은 메모리 크기를 나타내고, len(rune[])은 길이를 나타낸다

--- main/main.go
+++ main/main.go
@@ -3,12 +3,31 @@ package main
 import "fmt"
 
 
+
 func main() {
-
-	var t[5]int = [5]int{1,2,3,4,5}
-
-	for i,v := range t{
-		fmt.Println(i, v)
-	}
 	
+	//Go String!
+	var char rune = '정'
+	fmt.Printf("%T\n", char)	
+	fmt.Printf("%c\n", char)
+	fmt.Println(char)
+
+	str1 := "가나다라마"
+	str2 := "abcde"
+	fmt.Println("==============================================")
+	fmt.Println(len(str1))
+	fmt.Println(len(str2))
+
+	str := "Hello World"
+	runes := []rune{72, 101, 108, 108, 111, 32, 87,111,114,108,100}
+	fmt.Println("==============================================")
+	fmt.Println(str)
+	fmt.Println(string(runes))
+
+	newstr := "안녕 나는 정다운입니다. haha~"
+	newrunes := []rune(newstr)
+
+	fmt.Println(len(newstr))
+	fmt.Println(len(newrunes))
+
 }
