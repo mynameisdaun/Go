fe199e87f9bc6ebb7e57a0829d50ed1e9741665c
mynameisdaun <daun9870.jung@gmail.com>
2021-10-29 23:18:05 +0900

    fill my grass

--- main/main.go
+++ main/main.go
@@ -2,36 +2,29 @@ package main
 
 import (
 	"fmt"
-	"strings"
 )
-
-func ToUpper1(str string) string {
-	var rst string
-	for _, c := range str {
-		if c >= 'a' && c <= 'z' {
-			rst += string('A' + ( c-'a'))
-		} else {
-			rst += string(c)
-		}
-	}
-	return rst
-}
-
-func ToUpper2(str string) string {
-	var builder strings.Builder
-	for _, c := range str {
-		if c >= 'a' && c <= 'z' {
-			builder.WriteRune('A'+(c-'a'))
-		} else {
-			builder.WriteRune(c)
-		}
-	}
-	return builder.String()
-}
 
 
 func main() {
 	
-	fmt.Println(ToUpper1("hihi"))
+	//many ways to copy array
+	slice1 := []int{1,2,3,4,5}
+	slice2 := make([]int, len(slice1))
 
+	for i,v := range slice1{
+		slice2[i]=v
+	}
+
+	slice3 := append([]int{},slice1...)
+	fmt.Println(slice3)
+
+	slice4 := make([]int, 4)
+	cnt1 := copy(slice4,slice1)
+	fmt.Println(cnt1)
+	fmt.Println(slice4)
+	fmt.Println("I want sleep!")
+
+
+
+	
 }
